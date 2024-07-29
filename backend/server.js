a0b62f1ed5d13bb541bb999e3a0123e09223b280
Rupak02 <rupaknbr@gmail.com>
2024-07-29 21:17:46 +0530

    c

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,10 @@ app.post("/api/contactDetails", async (req, res) => {
      const newUser = new User({ name, email, message });
       await newUser.save();
       res.status(200).json({ message: "Message received and saved to database" });
-  }). catch (err) {
+  }). catch ((err)=>{
     console.error(err);
     res.status(500).json({ message: "Failed to save message" });
-  }
+   })
 });
   
 
