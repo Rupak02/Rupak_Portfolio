cdb610c04dcd1afe7f95bbaf68ff75da3a4ad0a3
Rupak02 <rupaknbr@gmail.com>
2024-07-29 21:08:51 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.post("/api/contactDetails", async (req, res) => {
   const { name, email, message } = req.body;
 
-  try {
-    const newUser = new User({ name, email, message });
-    await newUser.save();
-    res.status(200).json({ message: "Message received and saved to database" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Failed to save message" });
-  }
+  
 });
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -48,11 +41,19 @@ app.post("/api/contactDetails", async (req, res) => {
     text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  transporter.sendMail(mailOptions, async (error, info) => {
     if (error) {
       console.log(error);
     } else {
       console.log("Email Sent Successfully: " + info.response);
+      try {
+        const newUser = new User({ name, email, message });
+        await newUser.save();
+        res.status(200).json({ message: "Message received and saved to database" });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Failed to save message" });
+      }
     }
   });
 
